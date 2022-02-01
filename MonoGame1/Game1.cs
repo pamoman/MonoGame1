0c921fdc1b8e7e976847d3a85121136abbbeb643
Paul Moreland <Paul.Moreland@edu.lidkoping.se>
2022-02-01 08:32:58 +0100

    Added esc option

--- MonoGame1/Game1.cs
+++ MonoGame1/Game1.cs
@@ -71,6 +71,11 @@ namespace MonoGame1
 
         protected override void Update(GameTime gameTime)
         {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
             DropRectangle();
 
             base.Update(gameTime);
