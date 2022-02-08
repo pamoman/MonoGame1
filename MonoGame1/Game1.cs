31ee46050314ca96d930e1c45b3e549cbb917b50
Paul Moreland <Paul.Moreland@edu.lidkoping.se>
2022-02-08 09:14:37 +0100

    Added Mario Jump

--- MonoGame1/Game1.cs
+++ MonoGame1/Game1.cs
@@ -10,13 +10,13 @@ namespace MonoGame1
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         SpriteFont arialFont;
-        Texture2D brick;
+        Texture2D brick, parrot;
 
         KeyboardState keyboardOne = Keyboard.GetState();
         KeyboardState keyboardTwo = Keyboard.GetState();
 
-        int windowWidth = 800;
-        int windowHeight = 480;
+        int windowWidth;
+        int windowHeight;
 
         string message = "Play Tetris!";
         double version = 0.1;
@@ -26,6 +26,11 @@ namespace MonoGame1
         List<Rectangle> bricks = new List<Rectangle>();
 
         int brickPos = 0;
+
+        Rectangle parrotRectangle;
+
+        int jumpHeight = 20, jumpCurrent = 20;
+        bool jump = false;
 
         public Game1()
         {
@@ -37,12 +42,15 @@ namespace MonoGame1
         {
             IsMouseVisible = true;
 
+            windowWidth = graphics.PreferredBackBufferWidth;
+            windowHeight = graphics.PreferredBackBufferHeight;
+
             for (int i = 0; i < 10; i++)
             {
                 bricks.Add(new Rectangle((100 + (i * 70)), -100, 60, 30));
             }
 
-            FullScreen();
+            /*FullScreen();*/
 
             base.Initialize();
         }
@@ -53,10 +61,10 @@ namespace MonoGame1
             graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
             graphics.IsFullScreen = true;
 
-            graphics.ApplyChanges();
-
             windowWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
             windowHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+            graphics.ApplyChanges();
         }
 
         protected override void LoadContent()
@@ -64,6 +72,9 @@ namespace MonoGame1
             spriteBatch = new SpriteBatch(GraphicsDevice);
             brick = Content.Load<Texture2D>("brick_grey");
             arialFont = Content.Load<SpriteFont>("arial");
+
+            parrot = Content.Load<Texture2D>("parrot");
+            parrotRectangle = new Rectangle(0, (windowHeight - (parrot.Height / 2)), parrot.Width / 2, parrot.Height / 2);
         }
 
         protected override void Update(GameTime gameTime)
@@ -73,9 +84,46 @@ namespace MonoGame1
                 Exit();
             }
 
-            PlayTetris(5, 10, 10);
+            /*PlayTetris(5, 10, 10);*/
+            PlayMario(5, 5);
 
             base.Update(gameTime);
+        }
+
+        protected void PlayMario(int left, int right)
+        {
+            keyboardTwo = keyboardOne;
+            keyboardOne = Keyboard.GetState();
+
+            if (jump && jumpCurrent > 0)
+            {
+                parrotRectangle.Y -= jumpCurrent;
+
+                jumpCurrent--;
+            }
+            else if (jumpCurrent < jumpHeight)
+            {
+                jumpCurrent++;
+
+                parrotRectangle.Y += jumpCurrent;
+
+                jump = false;
+            }
+
+            if (keyboardOne.IsKeyDown(Keys.Left) && (parrotRectangle.X > 0))
+            {
+                parrotRectangle.X -= left;
+            }
+
+            if (keyboardOne.IsKeyDown(Keys.Right) && (parrotRectangle.X < windowWidth - (parrot.Width / 2)))
+            {
+                parrotRectangle.X += right;
+            }
+
+            if ((keyboardOne.IsKeyDown(Keys.Space)) && (keyboardTwo.IsKeyUp(Keys.Space)))
+            {
+                jump = true;
+            }
         }
 
         protected void PlayTetris(int down, int left, int right)
@@ -92,17 +140,17 @@ namespace MonoGame1
 
                 if (!isTouching && (brickTemp.Y < windowHeight - brickTemp.Height))
                 {
-                    if (keyboardOne.IsKeyDown(Keys.Left) && brickTemp.X > 0)
+                    if (keyboardOne.IsKeyDown(Keys.Left) && (brickTemp.X > 0))
                     {
                         brickTemp.X -= left;
                     }
 
-                    if (keyboardOne.IsKeyDown(Keys.Right) && brickTemp.X < windowWidth - brickTemp.Width)
+                    if (keyboardOne.IsKeyDown(Keys.Right) && (brickTemp.X < windowWidth - brickTemp.Width))
                     {
                         brickTemp.X += right;
                     }
 
-                    if ((keyboardOne.IsKeyDown(Keys.Space) && keyboardTwo.IsKeyUp(Keys.Space)))
+                    if ((keyboardOne.IsKeyDown(Keys.Space)) && (keyboardTwo.IsKeyUp(Keys.Space)))
                     {
                         int width = brickTemp.Width;
                         int height = brickTemp.Height;
@@ -190,6 +238,8 @@ namespace MonoGame1
             {
                 spriteBatch.Draw(brick, b, Color.White);
             }
+
+            spriteBatch.Draw(parrot, parrotRectangle, Color.White);
 
             spriteBatch.End();
 
